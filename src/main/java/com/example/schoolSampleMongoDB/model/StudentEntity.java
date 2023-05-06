25cd23068768729a367ed4e0caa89ccd0003d0fb
YasamanTaleghani <y.taleghani@adpdigital.com>
2023-05-06 09:46:17 +0330

    add getter and setter to StudentEntity.java class

--- src/main/java/com/example/schoolSampleMongoDB/model/StudentEntity.java
+++ src/main/java/com/example/schoolSampleMongoDB/model/StudentEntity.java
@@ -25,4 +25,55 @@ public class StudentEntity {
         this.average = average;
         this.courseList = courseList;
     }
+
+    public StudentEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getAverage() {
+        return average;
+    }
+
+    public void setAverage(String average) {
+        this.average = average;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
 }
